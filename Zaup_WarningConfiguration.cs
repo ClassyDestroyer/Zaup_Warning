3505aad171624f38897f15d24b7da753a1db3f4a
ClassyDestroyer <admin@classynutgaming.xyz>
2016-01-11 20:34:30 +1100

    Create Zaup_WarningConfiguration.cs

---
+++ Zaup_WarningConfiguration.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using Rocket.API;
+
+namespace Zaup_Warning
+{
+    public class Zaup_WarningConfiguration : IRocketPluginConfiguration
+    {
+        public string DatabaseAddress;
+        public string DatabaseName;
+        public string DatabaseUsername;
+        public string DatabasePassword;
+        public ushort DatabasePort;
+        public string TableName;
+        public ulong KeepWarningsLengthDays;
+        public bool WarningtoKickOn;
+        public byte WarningstoKick;
+
+        public void LoadDefaults()
+        {
+            DatabaseAddress = "localhost";
+            DatabaseName = "unturned";
+            DatabaseUsername = "unturned";
+            DatabasePassword = "password";
+            DatabasePort = 3306;
+            TableName = "warnings";
+            KeepWarningsLengthDays = 30;
+            WarningtoKickOn = true;
+            WarningstoKick = 3;
+        }
+    }
+}
